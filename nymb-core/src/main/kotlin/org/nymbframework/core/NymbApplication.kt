a806fc5497dbabe2e4177e8362e4f0c4b2bfb8f0
Johan Palmfjord <johan.palmfjord@gmail.com>
2019-10-18 08:02:33 +0200

    init commit 🎉

---
+++ nymb-core/src/main/kotlin/org/nymbframework/core/NymbApplication.kt
@@ -0,0 +1,3 @@
+package org.nymbframework.core
+
+class NymbApplication
---
+++ nymb-core/src/test/kotlin/org/nymbframework/core/NymbApplicationTest.kt
@@ -0,0 +1,10 @@
+package org.nymbframework.core
+
+import org.junit.Test
+
+class NymbApplicationTest {
+    @Test
+    fun instantiatesNymbApplication() {
+        val app = NymbApplication()
+    }
+}
