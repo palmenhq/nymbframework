573e4efc5495492d6ed9f08356788a44ddca0666
Johan Palmfjord <johan.palmfjord@gmail.com>
2019-10-27 15:46:37 +0100

    Allow setting the log level

--- nymb-core/src/main/kotlin/org/nymbframework/core/NymbApplication.kt
+++ nymb-core/src/main/kotlin/org/nymbframework/core/NymbApplication.kt
@@ -1,16 +1,24 @@
 package org.nymbframework.core
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
 import org.nymbframework.bundles.check.CheckBundle
 import org.nymbframework.bundles.server.ServerBundle
 import org.nymbframework.core.commandline.RootCommand
 import org.nymbframework.core.configuration.PropertiesConfigurationReader
 import org.nymbframework.core.environment.Environment
+import org.slf4j.LoggerFactory
 import picocli.CommandLine
 
 class NymbApplication(
     val environment: Environment
 ) {
     fun run(vararg args: String): Int {
+        val logLevel = environment.configurationReader["log.level"]
+        if (logLevel != null) {
+            (LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as Logger).level = Level.valueOf(logLevel)
+        }
+
         val rootCommand = RootCommand()
         val commandLine = CommandLine(rootCommand)
         rootCommand.commandLine = commandLine
